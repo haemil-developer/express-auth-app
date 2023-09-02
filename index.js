53e2a59f3fc6dea072b42789966836b76d2323f6
haemil <every.haemil@gmail.com>
2023-09-02 22:35:04 +0900

    create accessToken

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const app = express();
+const secretText = 'secretText';
+
+const posts = [
+    {
+        username: 'Alice',
+        title: 'Wonder Land'
+    },
+    {
+        username: 'Harry',
+        title: 'Hogwarts'
+    }
+]
 
 app.use(express.json());
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const user = { name: username };
+
+    // create token using jwt   payload + secretText
+    const accessToken = jwt.sign(user, secretText);
+    res.json({ accessToken: accessToken });
+})
+
+app.get('/posts', (req, res) => {
+    res.json(posts);
+})
 
 const port = 3000;
 app.listen(port, () => {
